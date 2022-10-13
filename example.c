d3d6c56fb408183aac0ca8ef5b95d5cba481ac90
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 12:12:04 +0800

    Added failed malloc detection

--- example.c
+++ example.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   example.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdameros <tdameros@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 22:15:02 by tdameros          #+#    #+#             */
-/*   Updated: 2022/10/12 22:16:20 by tdameros         ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 12:11:30 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ int	main(void)
 {
 	void	*test1;
 	void	*test2;
+	void	*test3;
 
 	test1 = malloc(sizeof(int) * 20);
 	test2 = malloc(sizeof(char *) * 13);
+	test3 = malloc(SIZE_MAX);
 
 	show_leaks();
 	return (0);
